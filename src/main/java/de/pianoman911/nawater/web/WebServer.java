99777a607b1a3dd367b651538ffb212d9d1e453b
pianoman911 <pianofinn13@gmail.com>
2023-11-15 01:58:47 +0100

    Added js chart formatting

--- src/main/java/de/pianoman911/nawater/web/WebServer.java
+++ src/main/java/de/pianoman911/nawater/web/WebServer.java
@@ -1,5 +1,6 @@
 package de.pianoman911.nawater.web;
 
+import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 import de.pianoman911.nawater.NaWater;
 import de.pianoman911.nawater.web.api.DataQueryApi;
@@ -13,6 +14,25 @@ public class WebServer extends Thread {
 
     public WebServer(NaWater service) {
         this.service = service;
+    }
+
+    public static boolean checkCors(HttpExchange exchange) {
+        exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+        exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "*");
+        exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "*");
+        exchange.getResponseHeaders().add("Access-Control-Allow-Credentials", "true");
+        exchange.getResponseHeaders().add("Access-Control-Max-Age", "86400");
+        try {
+            if (exchange.getRequestMethod().equals("OPTIONS")) {
+
+                exchange.sendResponseHeaders(204, -1);
+                exchange.close();
+                return true;
+            }
+        } catch (Exception exception) {
+            throw new RuntimeException(exception);
+        }
+        return false;
     }
 
     @Override
--- src/main/java/de/pianoman911/nawater/web/api/DataQueryApi.java
+++ src/main/java/de/pianoman911/nawater/web/api/DataQueryApi.java
@@ -1,10 +1,12 @@
 package de.pianoman911.nawater.web.api;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import de.pianoman911.nawater.NaWater;
 import de.pianoman911.nawater.util.StreamUtils;
+import de.pianoman911.nawater.web.WebServer;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -20,6 +22,9 @@ public class DataQueryApi implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+        if (WebServer.checkCors(exchange)) {
+            return;
+        }
         try {
             String[] raw = exchange.getRequestURI().getQuery().split("&");
             Map<String, String> args = new HashMap<>();
@@ -32,14 +37,32 @@ public class DataQueryApi implements HttpHandler {
             String id = args.get("id");
             long start = Long.parseLong(args.get("start"));
             long end = Long.parseLong(args.get("end"));
+            boolean chartJs = false;
+            if (args.containsKey("chartjs")) {
+                chartJs = Boolean.parseBoolean(args.get("chartjs"));
+            }
 
             JsonArray array = this.service.getArchiver().query(id, start, end);
+
+            JsonArray jsInnerX = new JsonArray();
+            JsonArray jsInnerY = new JsonArray();
+            if (chartJs) {
+                for (int i = 0; i < array.size(); i++) {
+                    JsonObject object = array.get(i).getAsJsonObject();
+                    jsInnerX.add(object.get("timestamp").getAsLong());
+                    jsInnerY.add(object.get("height").getAsDouble());
+                }
+                array = new JsonArray();
+                array.add(jsInnerX);
+                array.add(jsInnerY);
+            }
 
             exchange.getResponseHeaders().add("Content-Type", "application/json");
             exchange.sendResponseHeaders(200, 0);
             StreamUtils.writeJsonFully(array, exchange.getResponseBody());
             exchange.getResponseBody().close();
         } catch (Exception exception) {
+            exception.printStackTrace();
             exchange.sendResponseHeaders(500, 0);
             exchange.getResponseBody().close();
         }
